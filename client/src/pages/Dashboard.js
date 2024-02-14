1f5e789015faf56741b98d0de47ad58b76b1a876
Emnetmamo <131816255+Emnetmamo@users.noreply.github.com>
2024-02-14 13:18:44 -0800

    Updated dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,33 +1,62 @@
-// src/components/Dashboard.js
+// src/components/DashboardPage.js
 
-import React, { useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import Sidebar from '../components/Sidebar';
-import Header from '../components/Header';
-import MainContent from '../components/MainContent';
+import React from 'react';
+import { Row, Col, Image } from 'react-bootstrap';
+import { BsPerson, BsBriefcase, BsGeoAlt, BsEnvelope, BsPhone } from 'react-icons/bs';
+import profileImg from '../assets/images/profileImg.jpg';
 
-const Dashboard = () => {
-  const [selectedPage, setSelectedPage] = useState('dashboard');
-
-  const handlePageChange = (page) => {
-    setSelectedPage(page);
-  };
-
+const DashboardPage = () => {
   return (
-    <div>
-      <Header />
-
-      <Container fluid>
+ 
         <Row>
-          <Col sm={3}>
-            <Sidebar onPageChange={handlePageChange} />
+          <Col md={3}>
+            <Image src={profileImg} alt="Profile" roundedCircle fluid />
           </Col>
-
-          <MainContent selectedPage={selectedPage} />
+          <Col md={9}>
+            <h2>Welcome to Admin's Dashboard</h2>
+            <Row className="mb-3">
+              <Col xs={1}>
+                <BsPerson />
+              </Col>
+              <Col>
+                <p>Abebe Tessema</p>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col xs={1}>
+                <BsBriefcase />
+              </Col>
+              <Col>
+                <p>Transport Routing Department</p>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col xs={1}>
+                <BsGeoAlt />
+              </Col>
+              <Col>
+                <p>Addis Ababa, Ethiopia</p>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col xs={1}>
+                <BsEnvelope />
+              </Col>
+              <Col>
+                <p>abebe@example.com</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={1}>
+                <BsPhone />
+              </Col>
+              <Col>
+                <p>+123 456 7890</p>
+              </Col>
+            </Row>
+          </Col>
         </Row>
-      </Container>
-    </div>
   );
 };
 
-export default Dashboard;
+export default DashboardPage;
